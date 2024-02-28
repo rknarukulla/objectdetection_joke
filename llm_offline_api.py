4c3e799756986928ab663ab1354fe27d30835169
ravinarukulla <n.ravikumar@live.com>
2024-02-28 10:36:15 +0530

    updated prompts

--- llm_offline_api.py
+++ llm_offline_api.py
@@ -5,16 +5,30 @@ from langchain_core.prompts import PromptTemplate
 
 app = Flask(__name__)
 
-# Assuming your model is loaded here (adjust according to your actual model loading code)
+# Initialize the model and template outside of the request handling function
 model_path = "llm_model/TheBloke/mistral-7b-instruct-v0.1.Q5_0/mistral-7b-instruct-v0.1.Q5_0.gguf"
-config = {'max_new_tokens': 100, 'temperature': 0}
+config = {'max_new_tokens': 100, 'temperature': 0.9}
 llm = CTransformers(model=model_path, model_file="mistral-7b-instruct-v0.1.Q5_0.gguf", config=config)
 
-template = """<s>[INST] You are a helpful, respectful and honest assistant. Answer exactly in less than 20 words from the context
-Answer the question below from context below :
-{context}
-{question} [/INST] </s>
+template0 = """<s>[INST] Imagine you are in a room where you've noticed several items and people around you, 
+. Based on this observation, craft a message that includes a 
+heartfelt wish and a compliment, making sure to incorporate the presence of these items and individuals creatively. 
+Ensure your response is both creative and formal, and do not refer to any process of detection or mention the 
+technical aspect of how you came to know about these items. [/INST] </s>"""
+
+
+template = """<s>[INST] You are a highly creative and articulate assistant with a formal tone. 
+Using the context provided, craft a response that includes a heartfelt wish and a genuine compliment. 
+Ensure your response maintains a balance between creativity and formality, 
+demonstrating respect and thoughtfulness.
+
+Context: {context}
+Question: {question} 
+[/INST] </s>
 """
+
+# Pre-define the prompt template to avoid recreating it on every request
+prompt_template = PromptTemplate(template=template, input_variables=["question", "context"])
 
 
 @app.route('/generate', methods=['POST'])
@@ -28,8 +42,7 @@ def generate_text():
     if not context_p or not question_p:
         return jsonify({"error": "Both 'context' and 'question' fields are required."}), 400
 
-    prompt = PromptTemplate(template=template, input_variables=["question", "context"])
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    llm_chain = LLMChain(prompt=prompt_template, llm=llm)
 
     response = llm_chain.invoke({"question": question_p, "context": context_p})
 
